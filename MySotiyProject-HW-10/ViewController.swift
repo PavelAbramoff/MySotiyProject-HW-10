dd9321b4571f8d6bcb7a4cd66a48ba391b68142c
PavelAbramoff <96954222+PavelAbramoff@users.noreply.github.com>
2022-01-29 20:39:26 +0100

    Initial Commit

---
+++ MySotiyProject-HW-10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySotiyProject-HW-10
+//
+//  Created by Pavel Абрамов on 29.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
